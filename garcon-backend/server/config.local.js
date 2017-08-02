e5d19dfd2940046f69bc7f555daabe2f046099c1
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-08-02 20:11:49 +0200

    Versioning api

---
+++ garcon-backend/server/config.local.js
@@ -0,0 +1,7 @@
+var p = require('../package.json');
+var version = p.version.split('.').shift();
+module.exports = {
+  restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 3000
+};
