02305ed8fc7f57c37d64d8fc7827fa41d56ccdeb
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-08-08 23:44:53 +0200

    Destroy backend

--- garcon-backend/server/config.local.js
+++
@@ -1,7 +0,0 @@
-var p = require('../package.json');
-var version = p.version.split('.').shift();
-module.exports = {
-  restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 3000
-};
