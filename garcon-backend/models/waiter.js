09b3575281b486b82d49dd41208aadeed2b97309
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-08-09 19:14:29 +0200

    Create a simple auth server with jwt token

---
+++ garcon-backend/models/waiter.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var mongoose = require ('mongoose');
+var bcrypt = require ('bcrypt');
+var Schema = mongoose.Schema;
+
+var WaiterSchema = new Schema({
+  fullName:{
+    type: String,
+    trim: true,
+    required: true
+  },
+  email:{
+    type: String,
+    unique: true,
+    trime: true,
+    lowercase: true,
+    required: true
+  },
+  hash_password:{
+    type: String,
+    required: true
+  },
+  create:{
+    type: Date,
+    default: Date.now
+  },
+  age:{
+    type: Number,
+    default: 18
+  },
+  sex:{
+    type: String,
+    default: "M"
+  }
+});
+
+WaiterSchema.methods.comparePassword = function(password){
+  return bcrypt.compareSync(password, this.hash_password);
+}
+
+module.exports = mongoose.model('Waiter', WaiterSchema);
--- garcon-backend/server.js
+++ garcon-backend/server.js
@@ -1,15 +1,24 @@
+'use strict';
+
+// Node modules
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require ('mongoose');
+var jsonwebtoken = require("jsonwebtoken");
+var bcrypt = require ('bcrypt');
+var morgan = require('morgan');
 
-var Bear = require('./models/bear');
+// Custom models
+var Waiter = require('./models/waiter');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/garcondb', {useMongoClient: true});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 var port = process.env.PORT || 3000;
 
@@ -24,35 +33,31 @@ router.get('/', function(req, res){
   res.json({ message: "garcon-backend api" });
 });
 
-/*
-router.post('/bears', function(req, res){
-  var bear = new Bear();
-  bear.name = req.body.name;
-
-  bear.save(function(err){
-    if(err) res.send(err);
-    else res.json({ message: "Bear " + bear.name + " created."})
+router.route('/auth/register').post(function(req, res){
+  var waiter = new Waiter(req.body);
+  waiter.hash_password = bcrypt.hashSync(req.body.password, 10);
+  waiter.save(function(err, user) {
+    if(err) return res.status(400).send({ message: err });
+    else {
+        user.hash_password = undefined;
+        return res.json(user);
+      }
   });
-
 });
-*/
 
-router.route('/bears')
-  .post(function(req, res){
-    var bear = new Bear();
-    bear.name = req.body.name;
-
-    bear.save(function(err){
-      if(err) res.send(err);
-      else res.json({ message: "Bear " + bear.name + " created."})
-    });
-  })
-  .get(function(req, res){
-    Bear.find(function(err, bears){
-      if(err) res.send(err);
-      else res.json(bears);
-    })
+router.route('/auth/login').post(function(req, res){
+  Waiter.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if(err) throw err;
+    if(!user) res.status(401).json({ message: 'Autenticazione fallita. Utente non trovato.' });
+    else if (user) {
+      if (!user.comparePassword(req.body.password)) res.status(401).json({ message: 'Autenticazione fallita. Password errata.' });
+      else return res.json({token: jsonwebtoken.sign({ email: user.email, fullName: user.fullName, _id: user._id}, 'RESTFULAPIs')});
+    }
   });
+
+})
 
 app.use('/api', router);
 
