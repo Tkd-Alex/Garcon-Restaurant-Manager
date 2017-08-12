46f895354fb0009e2739b24635a9abaf728a69aa
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-08-12 14:24:13 +0200

    Fix npm problem

--- garcon-react-native/src/store.js
+++ garcon-react-native/src/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
-import promiseMiddleware from 'redux-promise-middleware';
 import logger from 'redux-logger';
 
 import appReducer from './reducers';
 
-const store = createStore(appReducer, {}, applyMiddleware(thunk, logger, promiseMiddleware));
+const store = createStore(appReducer, {}, applyMiddleware(thunk, logger));
 
 export default store;
