eb4c41ec04e630afedbb997abb514225fea2c816
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-09-03 23:37:11 +0200

    Add script to fix watchman erro

---
+++ fix-watchman.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo 256 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
+echo 32768 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
+echo 65536 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
